649c9d79450cfeae8ad20085f5ae9c6c0fadb409
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-02 17:25:46 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'root#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :greetings, only: [:index, :show]
+      end
+    end
+  end
 end
